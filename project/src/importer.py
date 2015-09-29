ac2ca54a85599156e7084cfe8545b3b1deb1945b
Christian Mielers <mielecqs@CIP-PYTHON-04.vdi.ruhr-uni-bochum.de>
2015-09-29 10:39:57 +0200

    Files can now be imported as numpy arrays
    2 Functions have been added to the importer. vectorize(..) will transform
    the input data to vectorial data (currently VERY lossy), and
    as_numpy_array will accept the vectorized data to build a numpy array out
    of it.
    its)

--- project/src/importer.py
+++ project/src/importer.py
@@ -1,5 +1,6 @@
 import time
 import csv
+import numpy as np
 
 def read(path, limit=None):
 	with open(path) as file:
@@ -19,3 +20,23 @@ def read(path, limit=None):
 			X = float(data_point[7])
 			Y = float(data_point[8])
 			yield (date, category, descript, day_of_week, pd_district, resolution, adress, X, Y)
+
+def vectorize(data):
+	crime_type_ids = {}
+	crime_type_counter = 0
+	
+	for data_point in data:
+		try:
+			crime_type_id = crime_type_ids[data_point[1]]
+		except KeyError:
+			crime_type_ids[data_point[1]] = crime_type_counter
+			crime_type_counter += 1
+			crime_type_id = crime_type_ids[data_point[1]]
+			
+		yield (crime_type_id, data_point[7], data_point[8])
+
+def to_numpy_array(data):
+	collected_data = [data_point for data_point in data]
+	return np.asarray(collected_data)
+	
+#to_numpy_array(vectorize(read('data/train.csv')))
