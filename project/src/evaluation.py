da047f30a7a235b49e4c8b37957e750f45795cd2
Christian Mielers <mielecqs@CIP-PYTHON-05.vdi.ruhr-uni-bochum.de>
2015-10-01 16:29:32 +0200

    A whole bunch 'o comments and docstrings

--- project/src/evaluation.py
+++ project/src/evaluation.py
@@ -1,13 +1,23 @@
 import math
 
 def logloss(predictions, truth):
-	ll = 0.0
+	'''Calculates and returns the log loss of the predictions, given that truth contains the true labels.
+	
+	Predictions is a numpy array of shape (#samples, #crime_types), providing the probabilites for each type
+	of crime as predicted for each sample. truth is a subscriptable, where the i-th component provides the
+	column index for the column corresponding to the correct crime. If it indicates a non-existing column,
+	all predictions for that sample are assumed to be false.
+	
+	The value is computed in accordance with
+	https://www.kaggle.com/c/sf-crime/details/evaluation
+	'''
+	ll = 0.0 # log loss
 	for i in range(predictions.shape[0]):
 		true_crime_id = truth[i]
 		try:
 			prob = predictions[i, true_crime_id]
-		except IndexError:
+		except IndexError: # This can be neccessary if the test set contains crime types that were not in the training set
 			prob = 0
-		prob = max( min(prob, 1 - 10**(-15)) , 10**(-15))
+		prob = max( min(prob, 1 - 10**(-15)) , 10**(-15)) # Avoid extreme values
 		ll += math.log(prob)
 	return (-1.0) * ll / predictions.shape[0]
--- project/src/importer.py
+++ project/src/importer.py
@@ -2,15 +2,23 @@ import time
 import csv
 import numpy as np
 
-def read(path, limit=None):
-	with open(path, 'r') as file: # FIXME change 2
-		descriptions = file.readline().split(',')
+def read_labled(path, limit=None):
+	'''Returns a sequence of tuples, each one representing one data point in the .csv file at path.
+	
+	This is intended for reading the training data . Each tuple is of the form
+	(date, category, descript, day_of_week, pd_district, resolution, adress, latitude, longitude)
+	where date is a time.struct_time object, category through adress are strings, and latitude and longitude are floats.
+	The number of data points read can be limited to a specific number with the limit parameter.
+	'''
+	with open(path, 'r') as file:
+		descriptions = file.readline().split(',') # The first line of the .csv file. We discard this.
 		csv_reader = csv.reader(file)
+		
 		for index, data_point in enumerate(csv_reader):
 			if limit is not None and index >= limit:
-				break
+				break # Yield no more data points
 			
-			date = time.strptime(data_point[0], '%Y-%m-%d %H:%M:%S')
+			date = time.strptime(data_point[0], '%Y-%m-%d %H:%M:%S') # Convert date string to time.struct_time
 			category = data_point[1]
 			descript = data_point[2]
 			day_of_week = data_point[3]
@@ -22,27 +30,49 @@ def read(path, limit=None):
 			yield (date, category, descript, day_of_week, pd_district, resolution, adress, latitude, longitude)
 
 def read_unlabled(path, limit=None):
+	'''Returns a sequence of tuples, each one representing one data point in the .csv file at path.
+	
+	This is intended for reading the test data. Each tuple is of the form
+	(date, day_of_week, pd_district, adress, latitude, longitude)
+	where date is a time.struct_time object, day_of_week, pd_district and adress are strings, and
+	latitude and longitude are floats.
+	The number of data points read can be limited to a specific number with the limit parameter.
+	'''
 	with open(path, 'r') as file:
 		descriptions = file.readline().split(',')
 		csv_reader = csv.reader(file)
+		
 		for index, data_point in enumerate(csv_reader):
 			if limit is not None and index >= limit:
-				break
+				break # Yield no more data points
 			
-			date = time.strptime(data_point[1], '%Y-%m-%d %H:%M:%S')
+			date = time.strptime(data_point[1], '%Y-%m-%d %H:%M:%S') # Convert date string to time.struct_time
 			day_of_week = data_point[2]
 			pd_district = data_point[3]
 			adress = data_point[4]
 			latitude = float(data_point[5])
 			longitude = float(data_point[6])
-			yield(date, day_of_week, pd_district, adress, latitude, longitude)
+			yield (date, day_of_week, pd_district, adress, latitude, longitude)
 			
 def to_numpy_array(data):
+	'''Generates and returns a single numpy array from the sequence of vectors in data.'''
 	collected_data = [data_point for data_point in data]
 	return np.asarray(collected_data)
 
 def write(path, predictions, crime_to_id):
-	# id_to_crime_dict = {value: key for key, value in crime_to_id_dict}
+	'''Writes the predictions to a .csv file at path, with order given by crime_to_id.
+	
+	path indicates a storage location on disk.
+	predictions is expected to be a numpy array containing probability predictions for each sample as its rows.
+	crime_to_id is expected to behave like a dictionary with the crime strings as keys and the ids as values. It
+	should map a crime string to the column in the predictions parameter where its probability is stored, e.g.
+	crime_to_id['THEFT / LARCENY'] == 4 if the probabilities for theft and larceny are stored in probabilites[:,6].
+	
+	From this, this function constructs a file on disk that contains the appropriate header, ids and probabilities
+	in the format required by kaggle.
+	'''
+	
+	# Liste of crime types ordered by their id. This is because predictions has its columns sorted by id as well.
 	first_line = [crime for crime in sorted(crime_to_id, key=crime_to_id.get)]
 	first_line.insert(0, 'Id')
 
@@ -55,33 +85,17 @@ def write(path, predictions, crime_to_id):
 			row.insert(0, index)
 			csv_writer.writerow( row )
 
-# path = 'data/test.csv'
-# data = read_unlabled(path, limit=10)
-# for d in data:
-	# print(d)
 
-# Show lower and upper limit of raw vs. normalized time of day
-# data = to_numpy_array(vectorize(read('data/train.csv', 10000)))
-# print('Min: {0} Max: {1}'.format(min(data[:,1]), max(data[:,1])))
-# rescaled_data = ensure_unit_variance(data)
-# print('Min: {0} Max: {1}'.format(min(rescaled_data[:,1]), max(rescaled_data[:,1])))
 
-# Plot histogram of raw vs. rescaled time of day
-# import matplotlib.pyplot as plt
-# fig, (ax0, ax1) = plt.subplots(ncols=2, figsize=(8, 4))
-# ax0.hist(data[:,1], 20)
-# ax0.set_title('Raw')
-# ax1.hist(rescaled_data[:,1], 20)
-# ax1.set_title('Rescaled')
-# plt.show()
 
-# Print counts for different crimes
-# crime_counter = {}
-# for data_point in read('data/train.csv', 10000):
-	# try:
-		# crime_counter[data_point[1]] += 1
-	# except KeyError:
-		# crime_counter[data_point[1]] = 1
 
-# for key, value in crime_counter.items():
-	# print('{0}: {1}'.format(key, value))
+
+
+
+
+
+
+
+
+
+
